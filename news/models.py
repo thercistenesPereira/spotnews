3e8a6fa2715a459b9d471fb3e3c06a5c9942c99e
thercistenes <thercistenes1@outlook.com>
2024-09-29 20:13:26 -0300

    2 - Crie a migrate e a model User

--- news/models.py
+++ news/models.py
@@ -13,3 +13,31 @@ class Category(models.Model):
             raise ValidationError(
                 "O nome não pode ter mais de 200 caracteres."
             )
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+    role = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    def clean(self):
+        if len(self.name) > 200:
+            raise ValidationError(
+                "O nome não pode ter mais de 200 caracteres."
+            )
+        if len(self.email) > 200:
+            raise ValidationError(
+                "O email não pode ter mais de 200 caracteres."
+            )
+        if len(self.password) > 200:
+            raise ValidationError(
+                "A senha não pode ter mais de 200 caracteres."
+            )
+        if len(self.role) > 200:
+            raise ValidationError(
+                "O papel não pode ter mais de 200 caracteres."
+            )
