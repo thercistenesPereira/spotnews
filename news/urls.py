066d7a9d1bfd01703114bb553386a20dc3308e48
thercistenes <thercistenes1@outlook.com>
2024-09-30 14:24:34 -0300

    5 - Crie a página de Detalhes de uma Notícia

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from news.views import index
+from news.views import index, news_details
 
 
 urlpatterns = [
     path("", index, name="home-page"),
+    path('news/<int:id>/', news_details, name='news-details-page'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from news.models import News
 
 
@@ -6,3 +6,8 @@ from news.models import News
 def index(request):
     news_list = News.objects.all()
     return render(request, "home.html", {'news_list': news_list})
+
+
+def news_details(request, id):
+    news_item = get_object_or_404(News, id=id)
+    return render(request, 'news_details.html', {'news': news_item})
