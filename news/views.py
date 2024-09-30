30843427c57142cbd3e555d6bb8943bcd1c27e86
thercistenes <thercistenes1@outlook.com>
2024-09-30 13:21:44 -0300

    4 - Crie a página Inicial

--- news/views.py
+++ news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from news.models import News
+
 
 # Create your views here.
+def index(request):
+    news_list = News.objects.all()
+    return render(request, "home.html", {'news_list': news_list})
